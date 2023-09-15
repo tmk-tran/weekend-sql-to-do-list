8631d1193c95bf7850888920234dc745ec9d13b6
T Mark <tmk1.tran@gmail.com>
2023-09-15 18:02:27 -0500

    added GET in client.js

--- server/public/script/client.js
+++ server/public/script/client.js
@@ -1,3 +1,18 @@
 $(() => {
-    console.log("JQ and JS ready!");
-  });
+  console.log("JQ and JS ready!");
+
+//   refreshList();
+//   onClick();
+});
+
+// GET list data
+function refreshList() {
+    $.ajax({
+      type: "GET",
+      url: "/list",
+    })
+      .then((response) => {
+        renderBooks(response);
+      })
+      .catch((err) => console.log(err));
+  }
