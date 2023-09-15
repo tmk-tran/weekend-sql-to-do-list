47bdd9c7f88aa1ffc9e3fea838c4e46bf244017e
T Mark <tmk1.tran@gmail.com>
2023-09-15 17:29:35 -0500

    GET/POST functional

--- server/route/router.js
+++ server/route/router.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../modules/pool");
 
+// GET
 router.get("/", (req, res) => {
   console.log("Received GET request at /list");
   const queryText = `SELECT * FROM "list" ORDER BY "id" ASC;`;
@@ -13,24 +14,47 @@ router.get("/", (req, res) => {
       console.log("error in GET request");
       res.sendStatus(500);
     });
+});
 
+// POST
 router.post("/", (req, res) => {
-    const list = req.body;
-    console.log("POST request", list);
-    const queryText = `INSERT INTO "list" ("task", "priority")
-                        VALUES ($1, $2)`;
-                        
-    pool
-    .query(queryText, [
-        list.task,
-        list.priority
-    ])
+  const list = req.body;
+  console.log("POST request at /list/ with data of", list);
+  const queryText = `INSERT INTO "list" ("task", "priority")
+                            VALUES ($1, $2)`;
+
+if (!list.task || !list.priority) {
+    res.sendStatus(400);
+    return;
+}
+
+  pool
+    .query(queryText, [list.task, list.priority])
     .then(() => res.sendStatus(201))
     .catch((err) => {
       console.log("error in POST request", err);
       res.sendStatus(500);
-    })
+    });
+});
 
+// DELETE 
+router.delete("/:id", (req, res) => {
+    const id = req.params.id;
+    console.log("DELETE requests at /list/ with id of", id);
+    const queryText = `DELETE FROM "list" WHERE "id" = $1;`;
+  
+    if (!id) {
+      res.sendStatus(400);
+      return;
+    }
+  
+    pool
+      .query(queryText, [id])
+      .then(() => res.sendStatus(204))
+      .catch((err) => {
+        console.log("error in DELETing item from list table", err);
+        res.sendStatus(500);
+      });
 });
 
 module.exports = router;
