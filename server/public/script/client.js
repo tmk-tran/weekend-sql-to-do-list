1a63666e80a1b01278430667c02883cf63b6c42c
T Mark <tmk1.tran@gmail.com>
2023-09-16 09:54:21 -0500

    added append function

--- server/public/script/client.js
+++ server/public/script/client.js
@@ -13,7 +13,7 @@ function refreshList() {
     url: "/list",
   })
     .then((response) => {
-      renderBooks(response);
+      appendList(response);
     })
     .catch((err) => console.log(err));
 }
@@ -22,3 +22,25 @@ function refreshList() {
 function onClick() {
     $("#submitBtn").on("click", handleSubmit);
 }
+
+function appendList(list) {
+  console.log('in appendList');
+
+  $("#viewList").empty();
+  for (let i = 0; i < list.length; i += 1) {
+    let item = list[i];
+    // For each item, append a new row
+    $("#viewList").append(`
+      <tr>
+        <td>${item.task}</td>
+        <td>${item.description}</td>
+        <td>${item.due}</td>
+        <td>${item.priority}</td>
+        <td>${item.notes}</td>
+        <td>${item.status}</td>
+        <td><button class="readyButton" data-id=${item.id}  data-ready=${item.ready} >${item.ready ? "Incomplete": "Complete"}</button></td>
+        <td><button class="deleteButton" data-id=${item.id}>Delete</button></td>
+      </tr>
+    `);
+  }
+};
