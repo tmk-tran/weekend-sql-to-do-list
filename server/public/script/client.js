d58e171910718c5ab511d0344829dc7cec11c34c
T Mark <tmk1.tran@gmail.com>
2023-09-17 04:17:56 -0500

    added delete modal, click event, html layout. tested modal and delete button functions properly

--- server/public/script/client.js
+++ server/public/script/client.js
@@ -1,5 +1,5 @@
 $(() => {
-  console.log("JQ and JS ready!");
+  console.log("JQ and JS ready!, Let's make a to-do list!");
 
   getList();
   onClick();
@@ -7,16 +7,43 @@ $(() => {
 
 // Click handlers
 function onClick() {
-  $( '#viewList' ).on( "click", ".completeButton", updateList);
-  $( '#viewList' ).on( "click", ".deleteButton", deleteTask);
+  $("#viewList").on("click", ".completeButton", updateList);
+  // $( '#viewList' ).on( "click", ".deleteButton", deleteTask); // Originally had, modified for delete modal
+  $("#viewList").on("click", ".deleteButton", function (event) {
+    const id = $(event.target).data("id");
 
-  $("#addBtn").on( 'click', function(){
+    // Store the ID of the item to be deleted
+    $("#confirmDeleteButton").data("id", id);
+
+    // Open the modal
+    $("#deleteConfirmationModal").modal("show");
+  });
+
+  $("#confirmDeleteButton").on("click", function (event) {
+    const id = $(event.target).data("id"); // get the ID of the item to be deleted
+
+    // Perform the delete usig ajax
+    $.ajax({
+      method: "DELETE",
+      url: `/list/${id}`,
+    })
+      .then(() => {
+        $("#deleteConfirmationModal").modal("hide"); // close the modal
+        alert("Removed from list!");
+        getList(); // update the list after deletion
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
+  $("#addBtn").on("click", function () {
     let task = $("#taskIn").val();
     let description = $("#descriptionIn").val();
     let priority = $("#priorityIn").val();
     let notes = $("#notesIn").val();
 
-    console.log( 'clicked add button' );
+    console.log("clicked add button");
 
     let itemSend = {
       task: task,
@@ -25,29 +52,31 @@ function onClick() {
       notes: notes,
     };
     // validation
-    if (!task || !priority){
-      alert("Please fill in all of the fields and try again!")
-      return
-    };
+    if (!task || !priority) {
+      alert("Please fill in all of the fields and try again!");
+      return;
+    }
 
-    $("#taskIn").val('');
-    $("#descriptionIn").val('');
+    $("#taskIn").val("");
+    $("#descriptionIn").val("");
     $("#priorityIn").val(); // set this to empty so it resets, with a letter
-    $("#notesIn").val('');
+    $("#notesIn").val("");
     // call saveTask with the new obejct
     saveTask(itemSend);
-  })
+  });
 }
 
+// Append function 
 function appendList(list) {
-  console.log('in appendList');
+  console.log("in appendList");
 
   $("#viewList").empty();
   for (let i = 0; i < list.length; i += 1) {
     let item = list[i];
 
     // Check if any of the fields are null or undefined
-    if (item.description === null || item.description === undefined) { // can rewrite to item.description = item.description || 'N/A';
+    if (item.description === null || item.description === undefined) {
+      // can rewrite to item.description = item.description || 'N/A';
       item.description = "N/A";
     }
 
@@ -58,17 +87,21 @@ function appendList(list) {
     // For each item, append a new row. Added class="task-row" to target w CSS later
     $("#viewList").append(`
       <tr class="task-row">
-        <td><button class="completeButton" data-id=${item.id}  data-ready=${item.complete} >${item.complete ? "Complete": "Incomplete"}</button></td>
+        <td><button class="completeButton" data-id=${item.id}  data-ready=${
+      item.complete
+    } >${item.complete ? "Complete" : "Incomplete"}</button></td>
         <td>${item.task}</td>
         <td>${item.description}</td>
         <td>${item.priority}</td>
         <td>${item.notes}</td>
-        <td><button class="deleteButton" data-toggle="modal" data-target="#deleteConfirmationModal" data-id=${item.id}>Delete</button>
+        <td><button class="deleteButton" data-toggle="modal" data-target="#deleteConfirmationModal" data-id=${
+          item.id
+        }>Delete</button>
         </td>
       </tr>
     `);
   }
-};
+}
 
 // GET list data
 function getList() {
@@ -83,21 +116,22 @@ function getList() {
     .catch((err) => console.log(err));
 }
 
-//ajax POST
-function saveTask(itemSend){
-  console.log('in saveTask', itemSend);
+// Ajax POST
+function saveTask(itemSend) {
+  console.log("in saveTask", itemSend);
 
   // ajax call to server to post task
- $.ajax({
-  method: "POST",
-  url: "/list",
-  data: itemSend,
- }).then(() => getList())
- .catch((err) => console.log(err));
-};
+  $.ajax({
+    method: "POST",
+    url: "/list",
+    data: itemSend,
+  })
+    .then(() => getList())
+    .catch((err) => console.log(err));
+}
 
-// ajax PUT
-function updateList(event){
+// Ajax PUT
+function updateList(event) {
   const id = $(event.target).data("id");
   const complete = $(event.target).data("ready");
   console.log(id, complete);
@@ -105,18 +139,22 @@ function updateList(event){
   $.ajax({
     method: "PUT",
     url: `/list/${id}`,
-    data: {complete: !complete},
+    data: { complete: !complete },
   })
-  .then(() => getList())
-  .catch((err) => {console.log("Error with PUT ajax", err)
-})
+    .then(() => getList())
+    .catch((err) => {
+      console.log("Error with PUT ajax", err);
+    });
 }
 
-const deleteTask = (event) => {
-  console.log('in deleteTask');
-  const id = $(event.target).data("id");
-  $.ajax({
-    method: "DELETE",
-    url: `/list/${id}`,
-  }).then(() => getList()).catch((err) => console.log(err));
-};
+// Original DELETE function, unneeded after modal added
+// const deleteTask = (event) => {
+//   console.log("in deleteTask");
+//   const id = $(event.target).data("id");
+//   $.ajax({
+//     method: "DELETE",
+//     url: `/list/${id}`,
+//   })
+//     .then(() => getList())
+//     .catch((err) => console.log(err));
+// };
