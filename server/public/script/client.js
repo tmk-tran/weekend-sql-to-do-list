94773b46bf722a4be6905cb8da0c44bfa029b0f8
T Mark <tmk1.tran@gmail.com>
2023-09-16 20:36:51 -0500

    added more functions and style

--- server/public/script/client.js
+++ server/public/script/client.js
@@ -32,7 +32,7 @@ function onClick() {
 
     $("#taskIn").val('');
     $("#descriptionIn").val('');
-    $("#priorityIn").val('');
+    $("#priorityIn").val(); // set this to empty so it resets, with a letter
     $("#notesIn").val('');
     // call saveTask with the new obejct
     saveTask(itemSend);
